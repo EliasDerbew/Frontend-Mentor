9cc2b38567cc93a1c66e4b85deca46baee114a61
Elias Derbew <eliasderbew1@gmail.com>
2024-10-06 00:19:50 +0300

    challenge 19 has done

---
+++ challenge 19/main.js
@@ -0,0 +1,9 @@
+const sidebarEl = document.querySelector('.side-bar')
+
+function showSidebar(){
+    sidebarEl.style.display = 'flex';
+}
+
+function hideSidebar(){
+    sidebarEl.style.display = 'none';
+}
